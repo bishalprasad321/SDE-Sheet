9dc24b1048ff366a4006cca9a4d84832ca411e3a
bishalprasad321 <bishalprasad321@gmail.com>
2023-07-08 08:09:09 +0530

    updated comments

--- Day 01/pascals_triangle.cpp
+++ Day 01/pascals_triangle.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Problem Description :
+-----------------------
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+   row = 1          1
+   row = 2         1 1
+   row = 3        1 2 1
+   row = 4       1 3 3 1
+   row = 5      1 4 6 4 1  
+
+Constraints :
+-------------
+1 <= numRows <= 30
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
